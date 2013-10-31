7286c7531e4b642cf041e0e8a40c4c8538befc55
Yosuke Ishikawa <y@ishkawa.org>
2013-10-31 21:55:32 +0900

    rewrite basic tests using OCUnit

---
+++ ISMemoryCacheTests/ISMemoryCacheTests.m
@@ -0,0 +1,63 @@
+#import <SenTestingKit/SenTestingKit.h>
+#import "ISMemoryCache.h"
+
+static NSString *ISTestKey = @"test";
+
+@interface ISMemoryCacheTests : SenTestCase {
+    ISMemoryCache *cache;
+}
+
+@end
+
+@implementation ISMemoryCacheTests
+
+- (void)setUp
+{
+    [super setUp];
+    cache = [[ISMemoryCache alloc] init];
+}
+
+- (void)tearDown
+{
+    cache = nil;
+    [super tearDown];
+}
+
+- (void)testSetObjectForKey
+{
+    NSObject *object = [[NSObject alloc] init];
+    [cache setObject:object forKey:ISTestKey];
+    STAssertEquals([cache objectForKey:ISTestKey], object, @"object for key should equal inserted object.");
+    STAssertTrue([[cache allKeys] containsObject:ISTestKey], nil);
+}
+
+- (void)testRemoveObjectForKey
+{
+    NSObject *object = [[NSObject alloc] init];
+    [cache setObject:object forKey:ISTestKey];
+    [cache removeObjectForKey:ISTestKey];
+    STAssertNil([cache objectForKey:ISTestKey], @"cache should not contain object for key.");
+    STAssertFalse([[cache allKeys] containsObject:ISTestKey], nil);
+}
+
+- (void)testRemoveUnretainedObjects
+{
+    @autoreleasepool {
+        NSObject *unretainedObject = [[NSObject alloc] init];
+        [cache setObject:unretainedObject forKey:ISTestKey];
+    }
+    [cache removeUnretainedObjects];
+    STAssertNil([cache objectForKey:ISTestKey], @"object for key should be removed.");
+}
+
+- (void)testNotRemoveRetainedObjects
+{
+    NSObject *retainedObject = [[NSObject alloc] init];
+    @autoreleasepool {
+        [cache setObject:retainedObject forKey:ISTestKey];
+    }
+    [cache removeUnretainedObjects];
+    STAssertEquals([cache objectForKey:ISTestKey], retainedObject, @"object for key should not be removed.");
+}
+
+@end
